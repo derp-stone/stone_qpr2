914d7b664def7f710b384fff01128edda5897aa0
Arijit78 <sahaarijit2007@gmail.com>
2024-02-27 09:57:10 +0000

    stone: Add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+# Clone Prebuilt Kernel
+git clone https://github.com/kamikaonashi/device_xiaomi_stone-kernel.git -b 14 device/xiaomi/stone-kernel
+
+# Clone Vendor
+git clone https://github.com/kamikaonashi/vendor_xiaomi_stone_unified.git -b 14 vendor/xiaomi
