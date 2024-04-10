7f8d5f71d5c130ab533214d2b986a81240a192e6
Arijit78 <sahaarijit2007@gmail.com>
2024-04-10 11:58:56 +0000

    Revert "[TEMP]stone: Fixup"
    This reverts commit 44c6f508b521e105787b4217a671b73f15c1e188.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 # Clone Kernel Source
-git clone https://github.com/crdroidandroid/android_kernel_qcom_sm8350.git -b 14.0 kernel/xiaomi/sm6375
+git clone https://github.com/Stone-Trees/kernel_xiaomi_sm6375.git -b 14 kernel/xiaomi/sm6375
 
 # Clone Prebuilt Kernel
 git clone https://github.com/kamikaonashi/device_xiaomi_stone-kernel.git -b 14 device/xiaomi/stone-kernel
@@ -11,8 +11,8 @@ git clone https://github.com/kamikaonashi/vendor_xiaomi_stone_unified.git -b 14 
 git clone --depth=1 https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git -b v4a packages/apps/ViPER4AndroidFX
 
 # Clone hardware xiaomi
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-21 hardware/xiaomi
+git clone https://github.com/Evolution-X/hardware_xiaomi.git -b udc hardware/xiaomi
 
 # Aosp 17.0.4 Clang
 #rm -rf prebuilts/clang/host/linux-x86/r498229b
-#git clone --depth=1 https://gitlab.com/itsshashanksp/android_prebuilts_clang_host_linux-x86_clang-r498229b.git -b 13.0 prebuilts/clang/host/linux-x86/r498229b
+git clone --depth=1 https://gitlab.com/itsshashanksp/android_prebuilts_clang_host_linux-x86_clang-r498229b.git -b 13.0 prebuilts/clang/host/linux-x86/r498229b
