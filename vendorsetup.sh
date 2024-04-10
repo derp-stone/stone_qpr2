9efc6c00b7cc3274b581603b489498f37a835988
Arijit78 <sahaarijit2007@gmail.com>
2024-04-10 12:04:20 +0000

    stone: Clone vendor from my org

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,7 +5,7 @@ git clone https://github.com/Stone-Trees/kernel_xiaomi_sm6375.git -b 14 kernel/x
 git clone https://github.com/kamikaonashi/device_xiaomi_stone-kernel.git -b 14 device/xiaomi/stone-kernel
 
 # Clone Vendor
-git clone https://github.com/kamikaonashi/vendor_xiaomi_stone_unified.git -b 14 vendor/xiaomi
+git clone https://github.com/Arijit78/vendor_xiaomi_stone.git -b 14 vendor/xiaomi
 
 # Clone v4a
 git clone --depth=1 https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git -b v4a packages/apps/ViPER4AndroidFX
