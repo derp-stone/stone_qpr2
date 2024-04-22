da98f7e60cd788f89871bb2f219e09d259c50aed
redgroul <redgroulman@gmail.com>
2024-04-22 09:14:46 +0300

    stone:derp-ify

--- derp_stone.mk
+++ derp_stone.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common ProjectMatrixx stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Inherit from stone device
 $(call inherit-product, device/xiaomi/stone/device.mk)
@@ -22,11 +22,6 @@ TARGET_DISABLE_EPPE := true
 TARGET_DEBLOAT := true
 SYSTEM_OPTIMIZE_JAVA := true
 SYSTEMUI_OPTIMIZE_JAVA := true
-MATRIXX_BUILD_TYPE := Official
-MATRIXX_MAINTAINER := ARIJIT-SAHA
-MATRIXX_CHIPSET := SM6375
-MATRIXX_BATTERY := 5000mah
-MATRIXX_DISPLAY := 1080x2400
 WITH_GMS := true
 EXTRA_GAPPS := false
 
@@ -34,7 +29,7 @@ EXTRA_GAPPS := false
 TARGET_USE_PIXEL_CHARGER := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_stone
+PRODUCT_NAME := derp_stone
 PRODUCT_DEVICE := stone
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
