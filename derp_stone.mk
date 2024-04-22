30792082f03f3a7a034f1419bb057e88ce2076b8
redgroul <redgroulman@gmail.com>
2024-04-22 22:14:52 +0300

    stone: cleanup

--- derp_stone.mk
+++ derp_stone.mk
@@ -15,14 +15,11 @@ $(call inherit-product, vendor/derp/config/common_full_phone.mk)
 $(call inherit-product, device/xiaomi/stone/device.mk)
 
 # Device props
-TARGET_ENABLE_BLUR := false
+TARGET_ENABLE_BLUR := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_DISABLE_EPPE := true
 TARGET_DEBLOAT := true
-SYSTEM_OPTIMIZE_JAVA := true
-SYSTEMUI_OPTIMIZE_JAVA := true
-
 # Charging Animation
 TARGET_USE_PIXEL_CHARGER := true
 
@@ -31,8 +28,9 @@ PRODUCT_NAME := derp_stone
 PRODUCT_DEVICE := stone
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
+DERP_BUILDTYPE := Official
 
 BUILD_FINGERPRINT := POCO/moonstone_p_global/moonstone:14/UKQ1.231003.002/V816.0.3.0.UMPMIXM:user/release-keys
-DERP_BUILDTYPE := Official
+
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
