116f0f48f26bb05c7f65a500701a5989a322b7a2
Ilya Valyuzhenich <lyaValyuzhenich@yandex.ru>
2024-07-13 16:02:14 +0300

    stone: drop duplicate BUILD_FINGERPRINT

--- derp_stone.mk
+++ derp_stone.mk
@@ -30,8 +30,6 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
 DERP_BUILDTYPE := Official
 
-BUILD_FINGERPRINT := POCO/moonstone_p_global/moonstone:14/UKQ1.231003.002/V816.0.3.0.UMPMIXM:user/release-keys
-
 BUILD_FINGERPRINT := POCO/moonstone_p_global/moonstone:14/UKQ1.231003.002/V816.0.6.0.UMPMIXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
