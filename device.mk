b6aa10e37982a115c6e967ac5be35734b19114a7
bgcngm <bgcngm@users.noreply.github.com>
2024-08-01 06:48:54 +0200

    stone: move to new RFS install_symlink targets

--- device.mk
+++ device.mk
@@ -19,6 +19,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
+# Add common definitions for Qualcomm
+$(call inherit-product, hardware/qcom-caf/common/common.mk)
+
 PRODUCT_SHIPPING_API_LEVEL := 30
 
 # A/B
