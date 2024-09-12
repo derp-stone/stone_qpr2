6d31d80f9f087711fb5e7abcf7b6200efc8da3ee
Ilya Valyuzhenich <lyaValyuzhenich@yandex.ru>
2024-09-12 19:23:43 +0300

    stone: derp: fix mistake

--- derp_stone.mk
+++ derp_stone.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common ProjectMatrixx stuff.
+# Inherit some common Derpfest stuff.
 $(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Inherit from stone device
