8c0973100f98f00cd034173a3775dffe36f7679e
Ilya Valyuzhenich <lyaValyuzhenich@yandex.ru>
2024-11-20 12:19:38 +0300

    stone: import oss powerhint

--- device.mk
+++ device.mk
@@ -531,10 +531,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     vendor.qti.hardware.perf@2.3.vendor
 
-PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/power/config/holi/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
     $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 PRODUCT_PACKAGES += \
